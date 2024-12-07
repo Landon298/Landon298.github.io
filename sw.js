2f406f02950c46365493b05e8822b827bd2944a7
Landon298 <2734805181@qq.com>
2024-12-07 20:29:23 +0800

    Site updated: 2024-12-07 20:29:22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1733574438066";const preCache = ["/hexo/images/taichi.png","/hexo/images/banner.webp","/hexo/css/loader.css","/hexo/css/style.css","/hexo/js/script.js"];const cacheDomain = [
+const VERSION = "1733574546310";const preCache = ["/hexo/images/taichi.png","/hexo/images/banner.webp","/hexo/css/loader.css","/hexo/css/style.css","/hexo/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
