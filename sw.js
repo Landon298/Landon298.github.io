38cf231cf38e3fd436911e7356c6bb471851cb26
“Landon298” <2734805181@qq.com>
2024-12-16 18:45:40 +0800

    Site updated: 2024-12-16 18:45:39

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1733633268566";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1734345935595";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
